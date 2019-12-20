e2d1f81d752fea084ce01026232d06d6e0e40788
Søren Kottal <snielsen43@gmail.com>
2019-12-20 13:23:16 +0100

    first

---
+++ Wox.Plugin.TailwindCss/TailwindCss.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Algolia.Search;
+using Newtonsoft.Json.Linq;
+
+namespace Wox.Plugin.Our.Umbraco
+{
+    internal class Main : IPlugin
+    {
+        public Main()
+        {
+        }
+
+        public void Init(PluginInitContext context)
+        {
+        }
+        
+        public string Truncate(string source, int length)
+        {
+            if (source.Length > length)
+            {
+                source = source.Substring(0, length) + "...";
+            }
+            var result = Regex.Replace(source, @"\r\n?|\n", " ");
+            return result;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            var algolia = new AlgoliaClient("BH4D9OD16A", "3df93446658cd9c4e314d4c02a052188");
+            var index = algolia.InitIndex("tailwindcss");
+            var search = query.Search;
+
+            if (search != "")
+            {
+                var results = index.Search(new Algolia.Search.Query(search).SetNbHitsPerPage(5));
+
+                var result = new List<Result>();
+
+                foreach (var item in results["hits"])
+                {
+                    var title = "";
+                    var subtitle = "";
+
+                    var levels = item["hierarchy"].Children<JProperty>().OrderBy(x => x.Name).Reverse();
+
+                    foreach (var level in levels)
+                    {
+                        if (level != null)
+                        {
+                            if (title == "")
+                            {
+                                title = level.Value.ToString();
+                            }
+                            else
+                            {
+                                subtitle = level.Value.ToString() + (subtitle != "" ? " - " + subtitle : "");
+                            }
+                        }
+                    }
+
+                    if (title != "")
+                    {
+                        result.Add(new Result()
+                        {
+                            Title = title,
+                            SubTitle = subtitle,
+                            IcoPath = "tailwind.png",
+                            Action = c =>
+                            {
+                                try
+                                {
+                                    Process.Start(item["url"].ToString());
+                                    return true;
+                                }
+                                catch (ExternalException e)
+                                {
+                                    MessageBox.Show("Open failed, please try later");
+                                    return false;
+                                }
+                            }
+                        });
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                return new List<Result>();
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Our Umbraco Wox plugin
+
+## Search our.umbraco.org for forumposts, projects or documentation using the /our keyboard.
+
+This is a plugin for [Wox](http://www.getwox.com/), a free and open source launcher app for Windows. It allows you to search our.umbraco.org for forumposts, projects or documentation in a quick and easy way.
+
+![](https://raw.githubusercontent.com/skttl/Wox.Plugin.Our.Umbraco/master/Readme/regular-search.png)
+
+## How to install
+Open your Wox prompt, and type
+`wpm install Our Umbraco`
+It's that simple!
+
+The plugin is now installed, and you can start using it.
+
+## Usage
+Open your Wox prompt, and type
+`/our`
+followed by your search query. Wox will fetch the top 5 results from our.umbraco.org, and display them to you. You can then click the desired one, and a browser will open on the selected page.
+
+### Narrowing search to specific content
+You can use /p, /f or /d as a second keyword, to narrow your search to a specific content type.
+
+`/our /f razor vs xslt`
+Searches the forums for threads mentioning "razor vs xslt"
+![](https://raw.githubusercontent.com/skttl/Wox.Plugin.Our.Umbraco/master/Readme/forum-search.png)
+
+`/our /p nested content`
+Searches for projects containing "nested content"
+![](https://raw.githubusercontent.com/skttl/Wox.Plugin.Our.Umbraco/master/Readme/projects-search.png)
+
+`/our /d installing`
+Searches for documentation regarding installing.
+![](https://raw.githubusercontent.com/skttl/Wox.Plugin.Our.Umbraco/master/Readme/documentation-search.png)
